d1ea0d41b100d7a6d78939d9bf72d3482af7735a
Jon Bjarne Bø <jonbjarnebo@gmail.com>
2023-01-19 23:29:20 +0100

    Completed projects overview

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from projects.models import Project
 
 # Create your views here.
-def project_list(request):
-    return render(request, 'projects/index.html')
+def all_projects(request):
+    # query te db to return all project objects
+    projects = Project.objects.all()
+    return render(request, 'projects/all_projects.html',
+                    {'projects': projects})
