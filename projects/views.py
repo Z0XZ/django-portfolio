5875d4a805d21470150ae094f3b23d6adcdb8189
Jon Bjarne Bø <jonbjarnebo@gmail.com>
2023-01-19 23:49:25 +0100

    same

--- projects/views.py
+++ projects/views.py
@@ -8,3 +8,8 @@ def all_projects(request):
     projects = Project.objects.all()
     return render(request, 'projects/all_projects.html',
                     {'projects': projects})
+
+def project_details(request, pk):
+    project = Project.objects.get(pk=pk)
+    return render(request, 'projects/details.html',
+    {'projects': projects})
